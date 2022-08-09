ba07da6043c75495cadfecfd5f6b8ead71e54e36
Chandru Kalahalamath <109204252+chandru151998@users.noreply.github.com>
2022-08-09 18:33:10 +0530

    UC5

---
+++ PrimeFactorisation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    internal class PrimeFactorisation
+    {
+        public static void Factorization()
+        {
+            Console.WriteLine("Enter the number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            while (num % 2 == 0)
+            {
+                Console.Write(2 + " ");
+                num /= 2;
+            }
+
+            for (int i = 3; i <= Math.Sqrt(num); i += 2)
+            {
+                while (num % i == 0)
+                {
+                    Console.Write(i + " ");
+                    num /= i;
+                }
+            }
+            if (num > 2)
+                Console.Write(num);
+        }
+    }
+}
